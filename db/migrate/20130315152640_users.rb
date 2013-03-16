cb72471621ac4786db97c47b3dcf996cb85ba0cd
Shivam Daryanani <daryananis@gmail.com>
2013-03-15 17:17:07 -0700

    Users & User validation

--- db/migrate/20130315152640_users.rb
+++ db/migrate/20130315152640_users.rb
@@ -1,4 +1,9 @@
 class Users < ActiveRecord::Migration
   def change
+  	create_table :users do |t|
+  		t.string :name, null: false
+  		t.string :email, null: false
+  		t.string :password_hash
+  	end
   end
 end
